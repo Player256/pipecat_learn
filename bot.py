7e26f4cf73fa67209b9e4775d6ba3cbfe4cccd43
Player256 <dattucodes@gmail.com>
2025-08-01 15:11:49 +0000

    ollama issue

--- bot.py
+++ bot.py
@@ -1,42 +1,23 @@
-#
-# Copyright (c) 2024–2025, Daily
-#
-# SPDX-License-Identifier: BSD 2-Clause License
-#
-
-import argparse
 import os
-
 import aiohttp
 from dotenv import load_dotenv
 from loguru import logger
-
-from pipecat.frames.frames import EndFrame, TTSSpeakFrame
 from pipecat.pipeline.pipeline import Pipeline
 from pipecat.pipeline.runner import PipelineRunner
 from pipecat.pipeline.task import PipelineTask
 from pipecat.services.rime.tts import RimeHttpTTSService
-from pipecat.transports.base_transport import BaseTransport, TransportParams
-from pipecat.transports.network.fastapi_websocket import FastAPIWebsocketParams
+from pipecat.services.deepgram.stt import DeepgramSTTService
+from pipecat.services.ollama.llm import OLLamaLLMService
+from pipecat.pipeline.task import PipelineParams, PipelineTask
+from pipecat.processors.aggregators.openai_llm_context import OpenAILLMContext
+from pipecat.transports.base_transport import TransportParams
 from pipecat.transports.network.small_webrtc import SmallWebRTCTransport
-from pipecat.transports.services.daily import DailyParams
 from pipecat.audio.vad.silero import SileroVADAnalyzer
 
 load_dotenv(override=True)
 
-# We store functions so objects (e.g. SileroVADAnalyzer) don't get
-# instantiated. The function will be called when the desired transport gets
-# selected.
-transport_params = {
-    "daily": lambda: DailyParams(audio_out_enabled=True),
-    "twilio": lambda: FastAPIWebsocketParams(audio_out_enabled=True),
-    "webrtc": lambda: TransportParams(audio_out_enabled=True),
-}
 
-
-async def run_example(
-    webrtc_connection
-):
+async def run_example(webrtc_connection):
     logger.info(f"Starting bot")
 
     transport = SmallWebRTCTransport(
@@ -44,31 +25,63 @@ async def run_example(
         params=TransportParams(
             audio_in_enabled=True,
             audio_out_enabled=True,
-            vad_analyzer=SileroVADAnalyzer(),  # Uncomment to enable VAD
-        )
+            vad_analyzer=SileroVADAnalyzer(),
+        ),
     )
 
-    # Create an HTTP session
     async with aiohttp.ClientSession() as session:
+
+        stt = DeepgramSTTService(api_key=os.getenv("DEEPGRAM_API_KEY"))
+
         tts = RimeHttpTTSService(
             api_key=os.getenv("RIME_API_KEY", ""),
             voice_id="rex",
             aiohttp_session=session,
         )
 
-        task = PipelineTask(Pipeline([tts, transport.output()]))
+        llm = OLLamaLLMService(model="llama3.1")
 
-        # Register an event handler so we can play the audio when the client joins
+        messages = [
+            {
+                "role": "system",
+                "content": "You are a helpful LLM in a WebRTC call. Your goal is to demonstrate your feymann-technique where you listen to the user's explanation of a topic and ask intuitive questions to help them clarify their understanding. You will not answer questions directly, but instead ask questions that lead the user to discover the answers themselves.",
+            },
+        ]
+
+        context = OpenAILLMContext(messages)
+
+        context_aggregator = llm.create_context_aggregator(context)
+
+        pipeline = Pipeline(
+            [
+                transport.input(),
+                stt,
+                context_aggregator.user(),
+                llm,
+                tts,
+                transport.output(),
+                context_aggregator.assistant(),
+            ]
+        )
+
+        task = PipelineTask(
+            pipeline,
+            params=PipelineParams(
+                enable_metrics=True,
+                enable_usage_metrics=True,
+            ),
+        )
+
         @transport.event_handler("on_client_connected")
         async def on_client_connected(transport, client):
-            await task.queue_frames([TTSSpeakFrame(f"Hello there!"), EndFrame()])
+            logger.info("Client Connected")
+            await task.queue_frames([context_aggregator.user().get_context_frame()])
+
+        @transport.event_handler("on_client_disconnected")
+        async def on_client_disconnected(transport, client):
+            logger.info(f"Client disconnected")
+            await task.cancel()
 
         runner = PipelineRunner(handle_sigint=False)
 
         await runner.run(task)
-
-
-# if __name__ == "__main__":
-#     from pipecat.examples.run import main
-
-#     main(run_example, transport_params=transport_params)
